20b8651a5a60bebba5adcd48d85fd21aab1fc154
Binbin Zhao <bizhao@vmware.com>
2015-12-27 13:19:31 +0800

    convert from csv to txt format

--- scripts/convert.py
+++ scripts/convert.py
@@ -1,27 +1,35 @@
 import sys
+import time
 
 if len(sys.argv) != 4:
-    print "Usage:", sys.argv[0], "stock_sypbol, source_csv_file, dest_txt_file"
+    print 'Usage:', sys.argv[0], 'stock_symbol, source_csv_file, dest_txt_file'
     exit(1)
 
-file csv  = open(sys.argv[2], "r")
-file dest = open(sys.argv[3], "w")
+csv  = open(sys.argv[2], 'r')
+dest = open(sys.argv[3], 'w')
 
-stock_sypbol = sys.argv[1]
+prefix = 'stock,symbol=' + sys.argv[1]
 
 for line in csv.readlines():
-    if "Date" in line:
+    if 'Date' in line:
         continue
 
     # Sample data in csv:
     # 2015-12-11,27.00,27.75,26.50,26.75,74803500,26.75
     # Want to convert to:
-    # stock,symbol=000917 open=27,high=27.75,low=26.5,close=26.75,volume=74803500,adj_close=26.75 1449817200
+    # stock,symbol=000917 open=27.00,high=27.75,low=26.50,close=26.75,volume=74803500,adj_close=26.75 1449817200
 
     values = line.split(',')
-    print "Original line:", line
+    # print 'Original line:', line
+    timestamp = int(time.mktime(time.strptime(values[0], '%Y-%m-%d')))
+    newline = prefix + ' open=' + values[1] + ',high=' + values[2] + ',low=' + values[3]
+    newline += ',close=' + values[4] + ',volume=' + values[5] + ',adj_close=' + values[6]
+    newline = newline.strip('\n') + ' ' + str(timestamp) + '\n'
+    # print 'New line:', newline
+    dest.write(newline)
 
-
+dest.close()
+csv.close()
 
 
 
